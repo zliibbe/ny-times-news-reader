66f3a782b404ba32365dafb992b597f0317e1128
Zach Liibbe <zliibbe@gmail.com>
2022-06-07 21:34:22 -0600

    Add footer

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -2,7 +2,11 @@ import React, { useState } from 'react'
 import './Footer.css'
 
 const Footer = () => {
-  return (  );
+  return ( 
+    <footer>
+      <h3> Copyright (c) 2022 The New York Times Company. All Rights Reserved.</h3>
+    </footer>
+   )
 }
 
 export default Footer
