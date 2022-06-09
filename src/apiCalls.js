01355a13c370a4d3c64be6477f6eace52fbf23b3
Zach Liibbe <zliibbe@gmail.com>
2022-06-09 14:10:32 -0600

    Add dynamic variable and error handling

--- src/apiCalls.js
+++ src/apiCalls.js
@@ -1,9 +1,15 @@
 const key = process.env.REACT_APP_API_KEY
 
-const fetchData = () => {
-  return fetch(`https://api.nytimes.com/svc/topstories/v2/home.json?api-key=${key}`)
-  .then(response => response.json())
-  .then(data => console.log(data))
+const fetchData = (type) => {
+  return fetch(`https://api.nytimes.com/svc/topstories/v2/${type}.json?api-key=${key}`)
+  .then((response) => {
+    if (!response.ok) {
+      throw new Error("Error fetching data. Please try again later");
+    } else {
+      return response.json();
+    }
+  })
+  // .then(data => console.log(data))
 }
 
 export default fetchData
