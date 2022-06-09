4c125253bf49d48a7e90eb3f0f551cac8358dbde
Zach Liibbe <zliibbe@gmail.com>
2022-06-09 14:13:35 -0600

    Add DataContext for Context API

---
+++ src/contexts/DataContext.js
@@ -0,0 +1,31 @@
+import React, { createContext, useState } from "react"
+import fetchData from '../apiCalls'
+
+const DataContext = createContext()
+
+const DataContextProvider = ({ children }) => {
+  const [articles, setArticles] = useState([]);
+  
+  const getData = (type) => {
+    fetchData(type)
+    .then(data => {
+      console.log("data: ", data);
+      setArticles(data.results)
+    } )
+  }
+
+ 
+  return (  
+    <DataContext.Provider
+      value={{
+        articles,
+        setArticles,
+        getData
+      }}
+    >
+      {children}
+    </DataContext.Provider>
+  );
+}
+ 
+export { DataContextProvider, DataContext }
