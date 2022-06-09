565dde993d66bd46cbc4b8abf341d4ef3ea90625
Zach Liibbe <zliibbe@gmail.com>
2022-06-09 14:11:47 -0600

    Add functional component article for list view

--- src/components/Article/Article.js
+++ src/components/Article/Article.js
@@ -1,8 +1,21 @@
-import React, { useState } from 'react'
+import React, { useState, Link } from 'react'
+// import Link from 'react-router-dom'
 import './Article.css'
 
-const Article = () => {
-  return (  );
+const Article = (props) => {
+  return ( 
+    
+    <div className='article'>
+      <div>
+        <img src={props.image}/>      
+      </div>
+      <div className='title-byline'>
+        <h2 className='title'>{props.title}</h2>
+        <p className='byline'>{props.byline}</p>
+      </div>
+    </div>
+
+   );
 }
  
-export default Article;
+export default Article
