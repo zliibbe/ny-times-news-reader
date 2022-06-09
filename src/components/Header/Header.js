db91565f914e3af2182e28f1440701be7262690d
Zach Liibbe <zliibbe@gmail.com>
2022-06-09 14:13:12 -0600

    Add JSX for header display

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,8 +1,16 @@
 import React, { useState } from 'react'
-import "./Header.css"
+import './Header.css'
+var dayjs = require('dayjs')
 
 const Header = () => {
-  return (  );
+  return ( 
+   <header>
+     <h1 className='site-title'>New York Times Reader</h1>
+     {/* <p className='header-date'>`See the top news on ${dayjs(Date.now(), 'MM-DD-YYYY')}`</p> */}
+   </header>
+
+
+   );
 }
  
 export default Header;
