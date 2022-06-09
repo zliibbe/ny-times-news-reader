b89890d1064ac5e92878aea1186abe1b30ca395a
Zach Liibbe <zliibbe@gmail.com>
2022-06-09 14:12:17 -0600

    Add WIP JSX for ArticleDetail

--- src/components/ArticleDetail/ArticleDetail.js
+++ src/components/ArticleDetail/ArticleDetail.js
@@ -2,8 +2,34 @@ import React, { useState } from 'react'
 import './ArticleDetail.css'
 
 
-const ArticleDetail = () => {
-  return (  );
+const ArticleDetail = (props) => {
+  return (
+    <div className='article-detail'>
+      <div>
+        <img href=""/>      
+      </div>
+      <div className=''>
+        <h2 className='title'>Title</h2>
+        <p className='abstract'>absract goes here</p>
+      </div>
+      
+    </div>
+  );
 }
  
 export default ArticleDetail;
+
+/*
+<div className='article'>
+      <h2 className='title'>{props.title}</h2>
+      <div>
+        <img href={props.image}/>      
+      </div>
+      <p className='article-abstract'>{props.abstract}</p>
+      <div>
+        <Link to={props.url}>
+          <button className='read-more-btn' type='button' >Read More</button>
+        </Link>
+      </div>
+    </div>
+    */
