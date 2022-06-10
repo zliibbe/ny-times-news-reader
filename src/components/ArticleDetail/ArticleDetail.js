5710dd9eeb034542c931871efd5c69b9c6be7f4b
Zach Liibbe <zliibbe@gmail.com>
2022-06-10 11:52:12 -0600

    Add sort for selectedArticle

--- src/components/ArticleDetail/ArticleDetail.js
+++ src/components/ArticleDetail/ArticleDetail.js
@@ -3,20 +3,23 @@ import { DataContext } from '../../contexts/DataContext'
 import './ArticleDetail.css'
 
 
-const ArticleDetail = (props) => {
+const ArticleDetail = ({articles, id}) => {
   const articleDetail = useContext(DataContext)
+  console.log("articleDetail: ", articleDetail);
+  const selectedArticle = articleDetail.articles.find(article => article.short_url.split('/')[3] === id)
 
+  console.log("selectedArticle: ", selectedArticle);
   return (
     <div className='article-detail'>
         <div className='image-container'>
-          <img src={articleDetail.image}/>      
+          {/* <img src={selectedArticle.image}/>       */}
         </div>
         <div className='title-byline-container'>
-          <h2 className='title'>{articleDetail.title}</h2>
-          <p className='byline'>{articleDetail.byline}</p>
+          <h2 className='title'>{selectedArticle.title}</h2>
+          <p className='byline'>{selectedArticle.byline}</p>
         </div>
-        <p className='abstract'>{articleDetail.abstract}</p>
-
+        <p className='abstract'>{selectedArticle.abstract}</p>
+        <a href={selectedArticle.url}>Read More</a>
     </div>
   );
 }
