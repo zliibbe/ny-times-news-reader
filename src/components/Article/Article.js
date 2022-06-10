41b2a7f320308b4b7bce2e452d253631c4b2d298
Zach Liibbe <zliibbe@gmail.com>
2022-06-09 22:13:41 -0600

    Add contatiners and NavLinks

--- src/components/Article/Article.js
+++ src/components/Article/Article.js
@@ -1,20 +1,23 @@
-import React, { useState, Link } from 'react'
-// import Link from 'react-router-dom'
+import React, { useState, useHistory } from 'react'
+import { NavLink } from "react-router-dom"
 import './Article.css'
 
 const Article = (props) => {
+  // const history = useHistory();
+
   return ( 
     
-    <div className='article'>
-      <div>
-        <img src={props.image}/>      
+    <NavLink to={`/${props.url}`}>
+      <div className='article' /*onClick={() => history.push(props.title)}*/>
+        <div className='image-container'>
+          <img src={props.image}/>      
+        </div>
+        <div className='title-byline-container'>
+          <h2 className='title'>{props.title}</h2>
+          <p className='byline'>{props.byline}</p>
+        </div>
       </div>
-      <div className='title-byline'>
-        <h2 className='title'>{props.title}</h2>
-        <p className='byline'>{props.byline}</p>
-      </div>
-    </div>
-
+    </NavLink>
    );
 }
  
