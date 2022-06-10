6acb4c291b93cca4beda088a55dba2db5ea13ba1
Zach Liibbe <zliibbe@gmail.com>
2022-06-10 11:50:49 -0600

    Add return for ArticleDetail render and routes

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -10,13 +10,17 @@ import Error from '../Error/Error'
 
 function App() {
   const articlesData = useContext(DataContext)
-  // const filterQuery = useContext()
 
-  console.log("articlesData: ", articlesData)
+  // console.log("articlesDataArr: ", articlesData.articles)
   useEffect(() => {
     articlesData.getData('home')
+    
   }, [])
   
+  // console.log("App0: ", articlesData)
+  // console.log("App1: ", articlesData.articles)
+  // console.log("App article2: ", articlesData.articles[0])
+  // console.log("App dig in3: ", articlesData.articles[0].multimedia);
 
 
   return (
@@ -24,17 +28,28 @@ function App() {
         <Layout>
           <Switch>
             <Route exact path="/" render={() => <ArticleContainer />}/>
-              
 
+            <Route exact path="/arts" render={() => <ArticleContainer />}/>
+
+            <Route exact path="/science" render={() => <ArticleContainer />}/>
+
+            <Route exact path="/us" render={() => <ArticleContainer />}/>
+
+            <Route exact path="/world" render={() => <ArticleContainer />}/>
+
+
+              
             <Route
-              path="/:article"
+              path="/:id"
               render={({match}) =>{
+                 return (
                  <ArticleDetail
+                 articles={articlesData}
+                 id={match.params.id}
                  url={match.params.url}/>
-              }}
+              )}}
             />
             
-
             <Route>
               <Error />
             </Route>
