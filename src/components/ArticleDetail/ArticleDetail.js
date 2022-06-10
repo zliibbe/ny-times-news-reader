5e8a8f8047454ada70c8bf4345be33a9fb14a14f
Zach Liibbe <zliibbe@gmail.com>
2022-06-10 13:26:52 -0600

    Add navigation buttons

--- src/components/ArticleDetail/ArticleDetail.js
+++ src/components/ArticleDetail/ArticleDetail.js
@@ -1,42 +1,32 @@
 import React, { useState, useContext } from 'react'
 import { DataContext } from '../../contexts/DataContext'
 import './ArticleDetail.css'
+import { useHistory } from 'react-router-dom'
 
 
 const ArticleDetail = ({articles, id}) => {
   const articleDetail = useContext(DataContext)
-  console.log("articleDetail: ", articleDetail);
   const selectedArticle = articleDetail.articles.find(article => article.short_url.split('/')[3] === id)
+  const history = useHistory()
 
-  console.log("selectedArticle: ", selectedArticle);
   return (
     <div className='article-detail'>
         <div className='image-container'>
           {/* <img src={selectedArticle.image}/>       */}
         </div>
-        <div className='title-byline-container'>
-          <h2 className='title'>{selectedArticle.title}</h2>
-          <p className='byline'>{selectedArticle.byline}</p>
-        </div>
-        <p className='abstract'>{selectedArticle.abstract}</p>
-        <a href={selectedArticle.url}>Read More</a>
+        <div className="">
+          <div className='title-byline-container'>
+            <h2 className='title'>{selectedArticle.title}</h2>
+            <p className='byline'>{selectedArticle.byline}</p>
+          </div>
+          <p className='abstract'>{selectedArticle.abstract}</p>
+          <div className='button-container'>
+            <a className='read-more-btn' href={selectedArticle.url} target="_blank">Read Full Article</a>
+            <div className='detail-back-button' onClick={history.goBack}>Go Back</div>
+          </div>
+          </div>  
     </div>
   );
 }
  
 export default ArticleDetail;
-
-/*
-<div className='article-detail'>
-      <h2 className='title'>{props.title}</h2>
-      <div>
-        <img href={props.image}/>      
-      </div>
-      <p className='article-abstract'>{props.abstract}</p>
-      <div>
-        <Link to={props.url}>
-          <button className='read-more-btn' type='button' >Read More</button>
-        </Link>
-      </div>
-    </div>
-    */
